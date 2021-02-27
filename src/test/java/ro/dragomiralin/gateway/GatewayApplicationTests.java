8550ef449f73dd2a1ca6ab21d7c43fff97b1ff7c
Dragomir Alin <dragomirdanielalin@gmail.com>
2021-02-27 21:01:02 +0200

    setup gateway

---
+++ src/test/java/ro/dragomiralin/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dragomiralin.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
