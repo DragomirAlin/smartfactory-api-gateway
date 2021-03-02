d5b2f2dc2f160c4a95ede25402f1c8e518cb0560
Dragomir Alin <dragomirdanielalin@gmail.com>
2021-03-02 21:00:47 +0200

    call microservices with loadbalancer

--- src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
+++ src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
@@ -1,12 +1,10 @@
 package ro.dragomiralin.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClientSpecification;
-import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -14,21 +12,16 @@ import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.client.RestTemplate;
 import ro.dragomiralin.gateway.filter.SimpleFilter;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
 @SpringBootApplication
 public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
-//	@Autowired(required = false)
-//	private List<RibbonClientSpecification> configurations = new ArrayList<>();
-//
 
 	@Bean @LoadBalanced
 	RestTemplate restTemplate() {
