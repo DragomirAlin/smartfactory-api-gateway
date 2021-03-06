a82661cafd5bac2b355576dd9f426b101bac5265
Dragomir Alin <dragomirdanielalin@gmail.com>
2021-03-06 13:29:09 +0200

    added dependency

--- src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
+++ src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +15,6 @@ import ro.dragomiralin.gateway.filter.SimpleFilter;
 @EnableZuulProxy
 @EnableEurekaClient
 @EnableFeignClients
-@EnableHystrixDashboard
 @SpringBootApplication
 public class GatewayApplication {
 	public static void main(String[] args) {
