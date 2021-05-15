f9a65249c91665b8ef81efa17a554f4839697be4
dragomiralin <dragomirdanielalin@yahoo.com>
2021-05-15 17:36:11 +0300

    fix ribbon, add ci, resolved #3

--- src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
+++ src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
@@ -14,7 +14,6 @@ import ro.dragomiralin.gateway.filter.SimpleFilter;
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-@Controller
 public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
