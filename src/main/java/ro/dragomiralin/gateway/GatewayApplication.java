1698913eab7a5f3f83eb210fa4aa03f53c5f4940
dragomiralin <dragomirdanielalin@yahoo.com>
2022-01-08 21:19:54 +0200

    delete circular dependency

--- src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
+++ src/main/java/ro/dragomiralin/gateway/GatewayApplication.java
@@ -1,19 +1,10 @@
 package ro.dragomiralin.gateway;
 
-import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.SwaggerUiConfigParameters;
-import org.springdoc.core.SwaggerUiConfigProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -22,20 +13,6 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-//	@Bean
-//	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters swaggerUiParameters, RouteDefinitionLocator routeDefinitionLocator) {
-//		List<GroupedOpenApi> groups = new ArrayList<>();
-//		List<RouteDefinition> definitions = routeDefinitionLocator.getRouteDefinitions().collectList().block();
-//		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-//			String name = routeDefinition.getId().replaceAll("-service", "");
-//			swaggerUiParameters.addGroup(name);
-//			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-//		});
-//		groups.forEach(defi -> {
-//			System.out.println(defi.getPathsToMatch());
-//		});
-//		return groups;
-//	}
 }
 
 
